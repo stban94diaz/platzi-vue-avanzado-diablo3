5c6335703601a2686286b6e64e2f2747e442c6b2
Esteban Danilo Diaz Riascos <stban94diaz@gmail.com>
2022-09-27 08:18:21 -0500

    :construction: add libraries and config bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// Bootstrap css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Use bootstrap
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
